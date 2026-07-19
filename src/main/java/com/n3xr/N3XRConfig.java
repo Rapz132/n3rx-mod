b81d61bba3aa8edfbf49263938c08427c041db17
Rapzyiè <herdiantorafka@gmail.com>
2026-07-19 17:53:33 +0700

    Update N3XRConfig.java

--- src/main/java/com/n3xr/N3XRConfig.java
+++ src/main/java/com/n3xr/N3XRConfig.java
@@ -6,6 +6,8 @@ public class N3XRConfig {
 	public static boolean showCps = false;
 	public static boolean showPing = false;
 
+	public static boolean nightVisionEnabled = false;
+
 	public static boolean snapEnabled = false;
 	public static boolean guidesEnabled = true;
 
@@ -24,8 +26,17 @@ public class N3XRConfig {
 	public static int fpsColorIndex = 0;
 	public static int cpsColorIndex = 0;
 	public static int pingColorIndex = 0;
+	public static int nightVisionColorIndex = 3;
 
 	public static int fpsColor() { return COLOR_PALETTE[fpsColorIndex]; }
 	public static int cpsColor() { return COLOR_PALETTE[cpsColorIndex]; }
 	public static int pingColor() { return COLOR_PALETTE[pingColorIndex]; }
+	public static int nightVisionColor() { return COLOR_PALETTE[nightVisionColorIndex]; }
+
+	public static final int[] DURATION_SECONDS = { 30, 60, 120, 300, -1 };
+	public static final String[] DURATION_NAMES = { "30s", "60s", "2min", "5min", "Infinite" };
+	public static int nightVisionDurationIndex = 4;
+
+	public static final String[] LEVEL_NAMES = { "I", "II", "III", "IV", "V" };
+	public static int nightVisionLevelIndex = 0;
 }
