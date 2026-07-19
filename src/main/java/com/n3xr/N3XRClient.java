09168cf3548b54db6503d681f0f3cdc015fbc0c2
Rapzyiè <herdiantorafka@gmail.com>
2026-07-19 11:56:57 +0700

    Update N3XRClient.java

--- src/main/java/com/n3xr/N3XRClient.java
+++ src/main/java/com/n3xr/N3XRClient.java
@@ -1,21 +1,43 @@
 package com.n3xr;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class N3XRClient implements ClientModInitializer {
 
+	private static KeyBinding openSettingsKey;
+
 	@Override
 	public void onInitializeClient() {
+		openSettingsKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.n3xr.settings",
+			InputUtil.Type.KEYSYM,
+			GLFW.GLFW_KEY_RIGHT_SHIFT,
+			"category.n3xr"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (openSettingsKey.wasPressed()) {
+				if (client.currentScreen == null) {
+					client.setScreen(new N3XRConfigScreen());
+				}
+			}
+		});
+
 		HudRenderCallback.EVENT.register((context, tickDelta) -> {
 			MinecraftClient mc = MinecraftClient.getInstance();
 			if (mc.player == null || mc.options.hudHidden) return;
 
-			renderFps(context, mc);
-			renderArmorHud(context, mc);
+			if (N3XRConfig.showFps) renderFps(context, mc);
+			if (N3XRConfig.showArmor) renderArmorHud(context, mc);
 		});
 	}
 
@@ -34,4 +56,4 @@ public class N3XRClient implements ClientModInitializer {
 			}
 		}
 	}
-}                     
+}											  
