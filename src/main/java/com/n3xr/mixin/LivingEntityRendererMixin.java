585a042c5014bed63c4090d313da84e51e0af59d
Rapzyiè <herdiantorafka@gmail.com>
2026-07-23 18:29:27 +0700

    Update LivingEntityRendererMixin.java

--- src/main/java/com/n3xr/mixin/LivingEntityRendererMixin.java
+++ src/main/java/com/n3xr/mixin/LivingEntityRendererMixin.java
@@ -2,8 +2,9 @@ package com.n3xr.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.n3xr.N3XRConfig;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
-public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState> {
+public abstract class LivingEntityRendererMixin<T extends LivingEntity> {
 
-	@Inject(method = "render", at = @At("HEAD"))
-	private void n3xr$beforeRender(S state, net.minecraft.client.util.math.MatrixStack matrices,
-	                                net.minecraft.client.render.VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		if (N3XRConfig.hitColorEnabled && ((LivingEntityRendererMixinAccessor) state).n3xr$isHurt()) {
+	@Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"))
+	private void n3xr$before(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+		int hurtTime = ((LivingEntityAccessor) entity).n3xr$getHurtTime();
+		if (N3XRConfig.hitColorEnabled && hurtTime > 0) {
 			int color = N3XRConfig.hitColor;
 			float r = ((color >> 16) & 0xFF) / 255f;
 			float g = ((color >> 8) & 0xFF) / 255f;
@@ -25,9 +26,8 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extend
 		}
 	}
 
-	@Inject(method = "render", at = @At("RETURN"))
-	private void n3xr$afterRender(S state, net.minecraft.client.util.math.MatrixStack matrices,
-	                                net.minecraft.client.render.VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+	@Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("RETURN"))
+	private void n3xr$after(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
 		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 	}
-  }
+}
