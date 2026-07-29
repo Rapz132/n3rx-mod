900e13e4ed7c8bac10dbc3e57d3de26e8ce2cb62
Rapzyiè <herdiantorafka@gmail.com>
2026-07-29 07:01:36 +0700

    Add new configuration options to N3XRConfigStorage

--- src/main/java/com/n3xr/N3XRConfigStorage.java
+++ src/main/java/com/n3xr/N3XRConfigStorage.java
@@ -19,13 +19,19 @@ public class N3XRConfigStorage {
 	private static class Data {
 		boolean showFps, showArmor, showCps, showPing, showKeystrokes, nightVisionEnabled;
 		boolean showServerIp, hitColorEnabled, zoomEnabled, showTps, showCompass;
-		boolean showSpeed, showCoords, customCrosshairEnabled, showNameTag;
+		boolean showSpeed, showCoords, customCrosshairEnabled, showNameTag, hitboxEnabled;
+		boolean autoGgEnabled, scoreboardHideEnabled, showPlayerCount, blockOutlineEnabled;
+		boolean showMemoryUsage, showCpuUsage, showBiomeInfo, showPotions, showInventoryDisplay;
+		boolean showRealTime, itemUpdateEnabled, noPumpkinOverlay, noFireOverlay;
 		boolean snapEnabled, guidesEnabled, keysRainbow, showFavoritesOnly;
+		int timezoneIndex;
 
 		int fpsX, fpsY, armorX, armorY, cpsX, cpsY, pingX, pingY, keysX, keysY;
 		int serverIpX, serverIpY, tpsX, tpsY, compassX, compassY, speedX, speedY, coordsX, coordsY, nameTagX, nameTagY;
+		int playerCountX, playerCountY, memoryX, memoryY, cpuX, cpuY, biomeX, biomeY, potionsX, potionsY, inventoryDisplayX, inventoryDisplayY, realTimeX, realTimeY;
 
-		int fpsColor, cpsColor, pingColor, keysColor, serverIpColor, hitColor, tpsColor, compassColor, speedColor, coordsColor, nameTagColor;
+		int fpsColor, cpsColor, pingColor, keysColor, serverIpColor, hitColor, tpsColor, compassColor, speedColor, coordsColor, nameTagColor, hitboxColor;
+		int playerCountColor, memoryColor, cpuColor, biomeColor, potionsColor, realTimeColor, blockOutlineColor;
 
 		int[] crosshairPixels;
 	}
@@ -47,10 +53,25 @@ public class N3XRConfigStorage {
 		d.showCoords = N3XRConfig.showCoords;
 		d.customCrosshairEnabled = N3XRConfig.customCrosshairEnabled;
 		d.showNameTag = N3XRConfig.showNameTag;
+		d.hitboxEnabled = N3XRConfig.hitboxEnabled;
+		d.autoGgEnabled = N3XRConfig.autoGgEnabled;
+		d.scoreboardHideEnabled = N3XRConfig.scoreboardHideEnabled;
+		d.showPlayerCount = N3XRConfig.showPlayerCount;
+		d.blockOutlineEnabled = N3XRConfig.blockOutlineEnabled;
+		d.showMemoryUsage = N3XRConfig.showMemoryUsage;
+		d.showCpuUsage = N3XRConfig.showCpuUsage;
+		d.showBiomeInfo = N3XRConfig.showBiomeInfo;
+		d.showPotions = N3XRConfig.showPotions;
+		d.showInventoryDisplay = N3XRConfig.showInventoryDisplay;
+		d.showRealTime = N3XRConfig.showRealTime;
+		d.itemUpdateEnabled = N3XRConfig.itemUpdateEnabled;
+		d.noPumpkinOverlay = N3XRConfig.noPumpkinOverlay;
+		d.noFireOverlay = N3XRConfig.noFireOverlay;
 		d.snapEnabled = N3XRConfig.snapEnabled;
 		d.guidesEnabled = N3XRConfig.guidesEnabled;
 		d.keysRainbow = N3XRConfig.keysRainbow;
 		d.showFavoritesOnly = N3XRConfig.showFavoritesOnly;
+		d.timezoneIndex = N3XRConfig.timezoneIndex;
 
 		d.fpsX = N3XRConfig.fpsX; d.fpsY = N3XRConfig.fpsY;
 		d.armorX = N3XRConfig.armorX; d.armorY = N3XRConfig.armorY;
@@ -63,6 +84,13 @@ public class N3XRConfigStorage {
 		d.speedX = N3XRConfig.speedX; d.speedY = N3XRConfig.speedY;
 		d.coordsX = N3XRConfig.coordsX; d.coordsY = N3XRConfig.coordsY;
 		d.nameTagX = N3XRConfig.nameTagX; d.nameTagY = N3XRConfig.nameTagY;
+		d.playerCountX = N3XRConfig.playerCountX; d.playerCountY = N3XRConfig.playerCountY;
+		d.memoryX = N3XRConfig.memoryX; d.memoryY = N3XRConfig.memoryY;
+		d.cpuX = N3XRConfig.cpuX; d.cpuY = N3XRConfig.cpuY;
+		d.biomeX = N3XRConfig.biomeX; d.biomeY = N3XRConfig.biomeY;
+		d.potionsX = N3XRConfig.potionsX; d.potionsY = N3XRConfig.potionsY;
+		d.inventoryDisplayX = N3XRConfig.inventoryDisplayX; d.inventoryDisplayY = N3XRConfig.inventoryDisplayY;
+		d.realTimeX = N3XRConfig.realTimeX; d.realTimeY = N3XRConfig.realTimeY;
 
 		d.fpsColor = N3XRConfig.fpsColor;
 		d.cpsColor = N3XRConfig.cpsColor;
@@ -75,6 +103,14 @@ public class N3XRConfigStorage {
 		d.speedColor = N3XRConfig.speedColor;
 		d.coordsColor = N3XRConfig.coordsColor;
 		d.nameTagColor = N3XRConfig.nameTagColor;
+		d.hitboxColor = N3XRConfig.hitboxColor;
+		d.playerCountColor = N3XRConfig.playerCountColor;
+		d.memoryColor = N3XRConfig.memoryColor;
+		d.cpuColor = N3XRConfig.cpuColor;
+		d.biomeColor = N3XRConfig.biomeColor;
+		d.potionsColor = N3XRConfig.potionsColor;
+		d.realTimeColor = N3XRConfig.realTimeColor;
+		d.blockOutlineColor = N3XRConfig.blockOutlineColor;
 
 		d.crosshairPixels = N3XRConfig.crosshairPixels;
 
@@ -106,10 +142,25 @@ public class N3XRConfigStorage {
 			N3XRConfig.showCoords = d.showCoords;
 			N3XRConfig.customCrosshairEnabled = d.customCrosshairEnabled;
 			N3XRConfig.showNameTag = d.showNameTag;
+			N3XRConfig.hitboxEnabled = d.hitboxEnabled;
+			N3XRConfig.autoGgEnabled = d.autoGgEnabled;
+			N3XRConfig.scoreboardHideEnabled = d.scoreboardHideEnabled;
+			N3XRConfig.showPlayerCount = d.showPlayerCount;
+			N3XRConfig.blockOutlineEnabled = d.blockOutlineEnabled;
+			N3XRConfig.showMemoryUsage = d.showMemoryUsage;
+			N3XRConfig.showCpuUsage = d.showCpuUsage;
+			N3XRConfig.showBiomeInfo = d.showBiomeInfo;
+			N3XRConfig.showPotions = d.showPotions;
+			N3XRConfig.showInventoryDisplay = d.showInventoryDisplay;
+			N3XRConfig.showRealTime = d.showRealTime;
+			N3XRConfig.itemUpdateEnabled = d.itemUpdateEnabled;
+			N3XRConfig.noPumpkinOverlay = d.noPumpkinOverlay;
+			N3XRConfig.noFireOverlay = d.noFireOverlay;
 			N3XRConfig.snapEnabled = d.snapEnabled;
 			N3XRConfig.guidesEnabled = d.guidesEnabled;
 			N3XRConfig.keysRainbow = d.keysRainbow;
 			N3XRConfig.showFavoritesOnly = d.showFavoritesOnly;
+			N3XRConfig.timezoneIndex = d.timezoneIndex;
 
 			N3XRConfig.fpsX = d.fpsX; N3XRConfig.fpsY = d.fpsY;
 			N3XRConfig.armorX = d.armorX; N3XRConfig.armorY = d.armorY;
@@ -122,6 +173,13 @@ public class N3XRConfigStorage {
 			N3XRConfig.speedX = d.speedX; N3XRConfig.speedY = d.speedY;
 			N3XRConfig.coordsX = d.coordsX; N3XRConfig.coordsY = d.coordsY;
 			N3XRConfig.nameTagX = d.nameTagX; N3XRConfig.nameTagY = d.nameTagY;
+			N3XRConfig.playerCountX = d.playerCountX; N3XRConfig.playerCountY = d.playerCountY;
+			N3XRConfig.memoryX = d.memoryX; N3XRConfig.memoryY = d.memoryY;
+			N3XRConfig.cpuX = d.cpuX; N3XRConfig.cpuY = d.cpuY;
+			N3XRConfig.biomeX = d.biomeX; N3XRConfig.biomeY = d.biomeY;
+			N3XRConfig.potionsX = d.potionsX; N3XRConfig.potionsY = d.potionsY;
+			N3XRConfig.inventoryDisplayX = d.inventoryDisplayX; N3XRConfig.inventoryDisplayY = d.inventoryDisplayY;
+			N3XRConfig.realTimeX = d.realTimeX; N3XRConfig.realTimeY = d.realTimeY;
 
 			N3XRConfig.fpsColor = d.fpsColor;
 			N3XRConfig.cpsColor = d.cpsColor;
@@ -134,10 +192,18 @@ public class N3XRConfigStorage {
 			N3XRConfig.speedColor = d.speedColor;
 			N3XRConfig.coordsColor = d.coordsColor;
 			N3XRConfig.nameTagColor = d.nameTagColor;
+			N3XRConfig.hitboxColor = d.hitboxColor;
+			N3XRConfig.playerCountColor = d.playerCountColor;
+			N3XRConfig.memoryColor = d.memoryColor;
+			N3XRConfig.cpuColor = d.cpuColor;
+			N3XRConfig.biomeColor = d.biomeColor;
+			N3XRConfig.potionsColor = d.potionsColor;
+			N3XRConfig.realTimeColor = d.realTimeColor;
+			N3XRConfig.blockOutlineColor = d.blockOutlineColor;
 
 			if (d.crosshairPixels != null) N3XRConfig.crosshairPixels = d.crosshairPixels;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-			}
+	}
