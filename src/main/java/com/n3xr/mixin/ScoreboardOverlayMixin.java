84539c8a2532ffa6939e861783d99562b66f04dd
Rapzyiè <herdiantorafka@gmail.com>
2026-07-29 19:26:17 +0700

    Update ScoreboardOverlayMixin.java

--- src/main/java/com/n3xr/mixin/ScoreboardOverlayMixin.java
+++ src/main/java/com/n3xr/mixin/ScoreboardOverlayMixin.java
@@ -3,7 +3,7 @@ package com.n3xr.mixin;
 import com.n3xr.N3XRConfig;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.scoreboard.ScoreboardDisplaySlot;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class ScoreboardOverlayMixin {
 
-    @Inject(method = "renderScoreboardSidebar", at = @At("HEAD"), cancellable = true)
+    @Inject(
+        method = "renderScoreboardSidebar",
+        at = @At("HEAD"),
+        cancellable = true
+    )
     private void n3xr$hideScoreboard(
             DrawContext context,
-            ScoreboardDisplaySlot slot,
+            RenderTickCounter tickCounter,
             CallbackInfo ci) {
 
         if (N3XRConfig.scoreboardHideEnabled) {
