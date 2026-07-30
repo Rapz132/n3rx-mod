7dd56a1d5460eaf5b169c6e2083cafeab7f249a3
Rapzyiè <herdiantorafka@gmail.com>
2026-07-30 09:08:11 +0700

    Create TitleScreenMixin.java

---
+++ src/main/java/com/n3xr/mixin/TitleScreenMixin.java
@@ -0,0 +1,34 @@
+package com.n3xr.mixin;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+
+    private static final Identifier BACKGROUND =
+            Identifier.of("n3xr", "textures/gui/main_menu_background.png");
+
+    @Inject(method = "renderPanoramaBackground", at = @At("HEAD"), cancellable = true)
+    private void n3xr$background(DrawContext context, float delta, CallbackInfo ci) {
+
+        context.drawTexture(
+                BACKGROUND,
+                0,
+                0,
+                0,
+                0,
+                context.getScaledWindowWidth(),
+                context.getScaledWindowHeight(),
+                context.getScaledWindowWidth(),
+                context.getScaledWindowHeight()
+        );
+
+        ci.cancel();
+    }
+}
