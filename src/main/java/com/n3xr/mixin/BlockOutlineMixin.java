48c7cd0e4e0e961a4f596f114124a1ef6d1fe021
Rapzyiè <herdiantorafka@gmail.com>
2026-07-31 13:02:43 +0700

    Update BlockOutlineMixin.java

--- src/main/java/com/n3xr/mixin/BlockOutlineMixin.java
+++ src/main/java/com/n3xr/mixin/BlockOutlineMixin.java
@@ -11,23 +11,21 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public class BlockOutlineMixin {
 
     @ModifyArgs(
-            method = "drawBlockOutline",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/WorldRenderer;drawShapeOutline(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/shape/VoxelShape;DDDFFFFZ)V"
-            ),
-            require = 1
+        method = "drawBlockOutline(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/entity/Entity;DDDLnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/render/WorldRenderer;drawShapeOutline(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/shape/VoxelShape;DDDFFFFZ)V"
+        ),
+        require = 1
     )
     private static void n3xr$colorOutline(Args args) {
-        if (!N3XRConfig.blockOutlineEnabled) {
-            return;
-        }
+        if (!N3XRConfig.blockOutlineEnabled) return;
 
         int color = N3XRConfig.blockOutlineColor;
 
-        float r = ((color >> 16) & 0xFF) / 255.0F;
-        float g = ((color >> 8) & 0xFF) / 255.0F;
-        float b = (color & 0xFF) / 255.0F;
+        float r = ((color >> 16) & 0xFF) / 255F;
+        float g = ((color >> 8) & 0xFF) / 255F;
+        float b = (color & 0xFF) / 255F;
 
         args.set(7, r);
         args.set(8, g);
