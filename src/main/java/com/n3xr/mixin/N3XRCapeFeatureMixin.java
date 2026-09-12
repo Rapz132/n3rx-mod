aceabd05c1549a7eb576cacd8bca2fec4905bc5f
Rapzyiè <herdiantorafka@gmail.com>
2026-09-13 06:58:57 +0700

    Update N3XRCapeFeatureMixin.java

--- src/main/java/com/n3xr/mixin/N3XRCapeFeatureMixin.java
+++ src/main/java/com/n3xr/mixin/N3XRCapeFeatureMixin.java
@@ -11,7 +11,7 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.CapeFeatureRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
